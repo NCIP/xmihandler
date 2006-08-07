b30f6a507019a05856803ca952c2a7e60222aa09
Christophe Ludet <ludetc@moxieinformatics.com>
2006-08-07 15:05:33 +0000

    javadoc
    SVN-Revision: 37

--- src/gov/nih/nci/ncicb/xmiinout/domain/UMLDependency.java
+++ src/gov/nih/nci/ncicb/xmiinout/domain/UMLDependency.java
@@ -2,6 +2,11 @@ package gov.nih.nci.ncicb.xmiinout.domain;
 
 import java.util.*;
 
+/**
+ * Represents a dependency between 2 UML Elements. 
+ * Usually represented by a dashed line in UML.
+ * <br> A Dependency has 2 ends, called client and supplier
+ */
 public interface UMLDependency extends UMLTaggableElement {
 
   /**
@@ -14,9 +19,16 @@ public interface UMLDependency extends UMLTaggableElement {
    */
   public UMLVisibility getVisibility();
 
-
+  /**
+   * 
+   * @return the client end
+   */
   public UMLDependencyEnd getClient();
 
+  /**
+   * 
+   * @return the supplier end
+   */
   public UMLDependencyEnd getSupplier();
 
 }
