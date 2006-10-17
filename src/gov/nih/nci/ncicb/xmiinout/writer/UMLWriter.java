b13a93aee213a6ef5ad775cf3e64b430369f0b2c
Christophe Ludet <ludetc@moxieinformatics.com>
2006-10-17 18:53:15 +0000

    assoc and assocEnd writer
    SVN-Revision: 62

--- src/gov/nih/nci/ncicb/xmiinout/writer/UMLWriter.java
+++ src/gov/nih/nci/ncicb/xmiinout/writer/UMLWriter.java
@@ -2,14 +2,13 @@ package gov.nih.nci.ncicb.xmiinout.writer;
 
 public interface UMLWriter {
   
-//   public void write(UMLModel model);
-//   public void write(UMLPackage pkg);
-//   public void write(UMLClass clazz);
-//   public void write(UMLAttribute attribute);
-//   public void write(UMLTaggedValue tv);
-
   public UMLClassWriter getUMLClassWriter();
+
   public UMLAttributeWriter getUMLAttributeWriter();
+
+  public UMLAssociationWriter getUMLAssociationWriter();
+
+  public UMLAssociationEndWriter getUMLAssociationEndWriter();
 
   public UMLTaggedValueWriter getUMLTaggedValueWriter();
 
