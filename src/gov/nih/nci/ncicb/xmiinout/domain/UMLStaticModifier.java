b6ff25631eee0100f9c6da26556dca40bd13cd8d
Prasad Konka <prasad.konka@nih.gov>
2013-04-10 10:11:05 -0400

    Update copyright

--- src/gov/nih/nci/ncicb/xmiinout/domain/UMLStaticModifier.java
+++ src/gov/nih/nci/ncicb/xmiinout/domain/UMLStaticModifier.java
@@ -1,6 +1,6 @@
 /*L
  *  Copyright Ekagra Software Technologies Ltd.
- *  Copyright Science Applications International Corporation
+ *  Copyright SAIC
  *
  *  Distributed under the OSI-approved BSD 3-Clause License.
  *  See http://ncip.github.com/xmihandler/LICENSE.txt for details.
